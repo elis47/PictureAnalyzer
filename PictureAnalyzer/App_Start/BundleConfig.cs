a4b66263c2a9fbfb458d8b4a48e728a5b860777c
Elisa <elisa.aioanei@yahoo.com>
2017-09-08 01:20:43 +0300

    almost finished the home page

--- PictureAnalyzer/App_Start/BundleConfig.cs
+++ PictureAnalyzer/App_Start/BundleConfig.cs
@@ -26,6 +26,8 @@ namespace PictureAnalyzer
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/Gallery").Include(
+                      "~/Content/Gallery.css"));
         }
     }
 }
