9839b9527dd78fd0eeb4138a4be6a8b55c120ddf
Elisa <elisa.aioanei@yahoo.com>
2017-09-15 16:38:26 +0300

    Added search functionality to home page

--- PictureAnalyzer/Controllers/HomeController.cs
+++ PictureAnalyzer/Controllers/HomeController.cs
@@ -1,16 +1,42 @@
-﻿using System;
+﻿using PictureAnalyzer.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace PictureAnalyzer.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private PictureAnalyzerDb db = new PictureAnalyzerDb();
+
+        public ActionResult Index(string searchString, int? page)
         {
-            return View();
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                return RedirectToAction("Search", new { SearchString = searchString, page = 1 });
+            }
+            else return View();
+        }
+
+        public ActionResult Search(string SearchString, int? page)
+        {
+            ViewBag.CurrentFilter = SearchString;
+
+            var galleries = from g in db.Galleries
+                            select g;
+            var painters = from p in db.Painters
+                           select p;
+
+            painters = painters.Where(p => p.Name.Contains(SearchString)
+                                       || p.Description.Contains(SearchString));
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            painters = painters.OrderBy(p => p.Name);
+
+            return View(painters.ToPagedList(pageNumber, pageSize));
         }
 
         public ActionResult About()
